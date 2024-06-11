a2219233b8f191d331beac7129baae9fb955ab98
ghulam qadar <ghulam.qadir@linentech.net>
2024-06-11 19:53:48 +0500

    update

--- cypress.config.js
+++ cypress.config.js
@@ -1,83 +1,36 @@
 const { defineConfig } = require("cypress");
 
-
 module.exports = defineConfig({
+  "env": {
+    "db": {
+      "host": "127.0.0.1",
+      "user": "root",
+      "password": "",
+      "database": "Testing_DataBase"
+    }
+  },
   e2e: {
     setupNodeEvents(on, config) {
-      // Implement node event listeners here
-      on("task", {
-        queryDb: (query) => {
-          return queryTestDb(query, config);
-        }
-      });
+      // implement node event listeners here
+      on('task', { queryDb: query => { return queryTestDb(query, config) }, }); //For running sql query
     },
-    env: {
-      "db": {
-        server: "127.0.0.1",
-        user: "root",
-        password: "",
-        database: "testing_databse"
-      }
-    }
-  }
+  },
 });
-const mysql = require('mysql');
 
+const mysql = require('mysql')
 function queryTestDb(query, config) {
-  const connection = mysql.createConnection(config.env.db);
-
+  // creates a new mysql connection using credentials from cypress.json env's
+  const connection = mysql.createConnection(config.env.db)
+  // start connection to db
+  connection.connect()
+  // exec query + disconnect to db as a Promise
   return new Promise((resolve, reject) => {
-    connection.connect();
-
     connection.query(query, (error, results) => {
-      if (error) {
-        reject(error);
-      } else {
-        connection.end();
-        resolve(results);
+      if (error) reject(error)
+      else {
+        connection.end()
+        return resolve(results)
       }
-    });
-  });
+    })
+  })
 }
-
-
-// module.exports = {
-//   retries: {
-//     experimentalStrategy: 'detect-flake-and-pass-on-threshold',
-//     experimentalOptions: {
-//       maxRetries: 2,
-//       passesRequired: 1,
-//     },
-
-//     // you must also explicitly set openMode and runMode to
-//     // either true or false when using experimental retries
-//     openMode: false,
-//     runMode: false,
-//   },
-//   // projectId: 'iz6rmd',
-//   // These settings apply everywhere unless overridden
-//   defaultCommandTimeout: 5000,
-//   viewportWidth: 1900,
-//   viewportHeight: 1000,
-//   // Viewport settings overridden for component tests // random used is 1000/660
-//   component: { //only for component testing
-//     viewportWidth: 1980,
-//     viewportHeight: 1920,
-//   },
-//   // Command timeout overridden for E2E tests
-//   e2e: {
-//     defaultCommandTimeout: 25000,
-//     "baseUrl": "https://linentech.net",
-//      slowTestThreshold : 10000,
-//      pageLoadTimeout: 50000,
-//      requestTimeout: 5000,
-//      video:false,
-//      responseTimeout: 5000,
-//      // failOnStatusCode: true,
-//      reporter: 'mochawesome',
-//      numTestsKeptInMemory: 0,
-//      testIsolation: true,
-  
-// },
-// }
-// // }
--- cypress/support/e2e.js
+++ cypress/support/e2e.js
@@ -15,7 +15,28 @@
 
 // Import commands.js using ES2015 syntax:
 import './commands'
-const mysql = require("mysql");
+//For connecting to SQL Server
+const mysql = require('mysql')
+function queryTestDb(query, config) {
+  // creates a new mysql connection using credentials from cypress.json env's
+  const connection = mysql.createConnection(config.env.db)
+  // start connection to db
+  connection.connect()
+  // exec query + disconnect to db as a Promise
+  return new Promise((resolve, reject) => {
+    connection.query(query, (error, results) => {
+      if (error) reject(error)
+      else {
+        connection.end()
+        return resolve(results)
+      }
+    })
+  })
+}
+module.exports = (on, config) => {
+  on('task', { queryDb: query => { return queryTestDb(query, config) }, }); //For running sql query
+}
+
 
 // Alternatively you can use CommonJS syntax:
 // require('./commands')
